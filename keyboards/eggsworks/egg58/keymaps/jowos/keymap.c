649f835a28d7e89d3dc850471af22f6aba2375f2
Yushi Sun <sunyushi9546@gmail.com>
2023-12-02 15:36:46 -0800

    lulu and eg58 layouts, wip

--- keyboards/eggsworks/egg58/keymaps/jowos/keymap.c
+++ keyboards/eggsworks/egg58/keymaps/jowos/keymap.c
@@ -3,18 +3,28 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer_names {
-    _BASE,
-    _FN
+
+enum layers {
+    _QWERTY,
+    _RAISE,
+    _LOWER,
+    _COLEMAK
 };
 
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+#define COLEMAK TG(_COLEMAK)
+#define ESCTL MT(MOD_LCTL, KC_ESC)
+#define SPCUP LT(_RAISE, KC_SPC)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT(
-  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,      KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_EQL,
-  KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,                    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,                    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSPC,
-            KC_LGUI, KC_LALT, OSL(1),  MO(1),   KC_SPC,                  KC_ENT,   MO(1), KC_LBRC, KC_RBRC, KC_BSLS
+[_QWERTY] = LAYOUT(
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+  ESCTL,    KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
+            KC_LGUI,KC_MINS, KC_EQL,  KC_LALT, SPCUP,                    KC_ENT,  KC_DEL,  KC_LBRC, KC_RBRC, KC_BSLS
 ),
 
 [_FN] = LAYOUT(
