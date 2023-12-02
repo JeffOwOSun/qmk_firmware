278cb607c3e8ada6595c720bd81d0a3a351301d8
Yushi Sun <sunyushi9546@gmail.com>
2023-12-02 15:36:46 -0800

    lulu and eg58 layouts, wip

--- keyboards/boardsource/lulu/keymaps/jowos/keymap.c
+++ keyboards/boardsource/lulu/keymaps/jowos/keymap.c
@@ -13,7 +13,7 @@ enum layers {
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 #define COLEMAK TG(_COLEMAK)
-#define ESCTL MT(MOD_LCTL, KC_ESC)
+#define ESCTL LCTL_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  LCTL_T(KC_ESC),    KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  ESCTL,    KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LOWER,   RAISE,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
                           KC_MINS,  KC_EQL, KC_LALT, KC_SPC,  KC_ENT,    KC_RGUI, KC_LBRC, KC_RBRC
 ),
